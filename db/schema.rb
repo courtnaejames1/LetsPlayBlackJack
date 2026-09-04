efd969fbcaa0bef8c23b70d0fa0e7bfb60d41fbb
courtnaejames1 <cimanijames@gmail.com>
2026-09-03 19:37:12 -0500

    adds database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_30_062823) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_021233) do
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "dealers", force: :cascade do |t|
     t.string "DealerName"
     t.decimal "HouseEarning"
